e8f89cfd40215acc791c6a970d15d078cc0661cd
amarthegr8 <amrendras420@gmail.com>
2025-07-10 22:50:49 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/logo.png" alt="Fortune Logo" style={{ height: 100 }} />
+        <h1>Fortune Field Tracker</h1>
+        <p>Welcome to the Admin Dashboard</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
